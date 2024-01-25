98b58c7f5b2efe0cd27cd418ece77e9ca4ce279f
Parth Rasal <parth.rasal@yahoo.com>
2024-01-25 21:35:23 +0530

    Create the game function
    Create a function to play the rounds 5 times and output the result
    for each round and the entire game.

--- index.js
+++ index.js
@@ -13,63 +13,90 @@ function playRound(computerSelection, playerSelection) {
         if (computerSelection == 'rock'){
             if (playerSelection == 'rock'){
                 play = true;
-                return ("It's a tie, both you and the computer entered " + playerSelection);
+                return 0;
             }
             else if (playerSelection == 'paper'){
                 play = false;
-                return ("You Win! " + playerSelection + " beats " + computerSelection + "!");
+                return 1;
             }
             else if (playerSelection == 'scissors'){
                 play = false
-                return ("You Lose! " + computerSelection + " beats " + playerSelection + "!");
+                return -1;
             }
             else {
                 play = false
-                return ("Enter a valid input");
+                return 10;
             }
         }
 
         if (computerSelection == 'paper'){
             if (playerSelection == 'paper'){
                 play = true;
-                return ("It's a tie, both you and the computer entered " + playerSelection);
+                return 0;
             }
             else if (playerSelection == 'scissors'){
                 play = false;
-                return ("You Win! " + playerSelection + " beats " + computerSelection + "!");
+                return 1;
             }
             else if (playerSelection == 'rock'){
                 play = false
-                return ("You Lose! " + computerSelection + " beats " + playerSelection + "!");
+                return -1;
             }
             else {
                 play = false
-                return ("Enter a valid input");
+                return 10;
             }
         }
 
         if (computerSelection == 'scissors'){
             if (playerSelection == 'scissors'){
                 play = true;
-                return ("It's a tie, both you and the computer entered " + playerSelection);
+                return 0;
             }
             else if (playerSelection == 'rock'){
                 play = false;
-                return ("You Win! " + playerSelection + " beats " + computerSelection + "!");
+                return 1;
             }
             else if (playerSelection == 'paper'){
                 play = false
-                return ("You Lose! " + computerSelection + " beats " + playerSelection + "!");
+                return -1;
             }
             else {
                 play = false
-                return ("Enter a valid input");
+                return 10;
             }
         }
     }
 
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(computerSelection, playerSelection));
+
+function game(){
+    var i = 0;
+    var playerScore = 0;
+    while (i < 5){
+        let playerSelection = prompt("Enter your choice: ", "Rock / Paper / Scissors");
+        result = game(getComputerChoice(), playerSelection);
+        if (result == 1){
+            alert("Your Win Round " + String(parseInt(i+1)) + "!");
+            i += 1;
+            player += 1;
+        }
+        else if (result == -1){
+            alert("Computer Wins Round " + String(parseInt(i+1)) + "!");
+            i += 1;
+        }
+        else if (result == 10){
+            alert("Please enter a valid input and try again!");
+        }
+    }
+
+    if (playerScore > 1){   
+        alert("You Win the Game!")
+    }
+    else{
+        alert("Try Again Later, Computer Wins!");
+    }
+}
+
+game();
